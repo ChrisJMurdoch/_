6cabefbca4bd7be1763b06bd74584e00764cca3f
Chris Murdoch <christopherjmurdoch@gmail.com>
2023-02-26 17:18:26 +0000

    Added configurable build mode

--- _.bat
+++ _.bat
@@ -1,13 +1,18 @@
 
 :: Utility script for managing CMake projects
-:: Chris Murdoch, 2022
+:: Chris Murdoch, 2023
 
 @echo off
 echo.
 
 SETLOCAL
 
+:: // ===== Project configurations
+
 set "BUILD_DIR=_"
+set "BUILD_MODE=Debug"
+
+:: \\ =====
 
 REM Get task argument
 set "task=%1"
@@ -22,7 +27,7 @@ if "%task%"=="init" (
 REM Use CMake to build the project
 ) else if "%task%"=="build" (
     if exist ".\%BUILD_DIR%\" (
-        cmake --build ./%BUILD_DIR%/ || (exit /b 1)
+        cmake --build ./%BUILD_DIR%/ --config %BUILD_MODE% || (exit /b 1)
     ) else (
         echo No buildsystem found.  Use _ init to generate buildsystem first.
     )
@@ -36,8 +41,8 @@ REM Clean all cmake data
 
 REM Run the debug executable
 ) else if "%task:~0,3%"=="run" (
-    if exist ".\%BUILD_DIR%\Debug\%executable%.exe" (
-        .\%BUILD_DIR%\Debug\%executable%.exe || (exit /b 1)
+    if exist ".\%BUILD_DIR%\%BUILD_MODE%\%executable%.exe" (
+        .\%BUILD_DIR%\%BUILD_MODE%\%executable%.exe || (exit /b 1)
     ) else (
         echo Executable '%executable%' found.  Use _ build to generate executable first.
     )
